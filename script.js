2e64c46d754dfe53668c66dd9743c1fa6d0abce9
Bruna Terribile <brunaterribile@gmail.com>
2022-09-01 11:15:27 -0300

    Criação do Quizz (telas 3A e 3B)

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-function createQuiz() {
+function createQuizz() { // Ao clicar em criar quizz, a tela 3A (info basicas) será apresentada
     alert('Oi');
+    const screen3 = document.querySelector('.screen3A');
+    screen3.classList.add('show-screen');
 }
