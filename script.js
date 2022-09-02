9b49a7d1cc0702ea60bfa351583556cfa5284089
Matheus Henrique <mathttps@gmail.com>
2022-09-02 15:48:43 -0300

    Layout dos quizzes com titulo(sem imagens)

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
+let allQuizzes = [];
+
+
 function createQuizz() { // Ao clicar em criar quizz, a tela 3A (info basicas) será apresentada
     alert('Oi');
     const screen3 = document.querySelector('.screen3A');
     screen3.classList.add('show-screen');
+}
+
+function carregarquizz(resposta){
+    allQuizzes = resposta.data;
+    renderizarQuizz(allQuizzes);
+}
+
+const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes")
+promise.then(carregarquizz);
+
+renderizarQuizz(allQuizzes)
+
+
+function renderizarQuizz(allQuizzes){
+
+    const quizz = document.querySelector('.test');
+    
+    quizz.innerHTML = ""
+
+    let teste;
+
+
+   quizz.innerHTML = `
+   <li class="rectangle">
+   <p class="tittle-quizz">${allQuizzes[].title}</p>
+</li>
+<li class="rectangle">
+<p class="tittle-quizz">${allQuizzes[2].title}</p>
+</li>  
+<li class="rectangle">
+<p class="tittle-quizz">${allQuizzes[1].title}</p>
+</li>             
+                `
 }
