aa4dc9c23a6e59a9fb552699365c43b6e457b635
Bruna Terribile <brunaterribile@gmail.com>
2022-09-04 18:51:02 -0300

    Divisão screens

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ function createQuizz() { // Ao clicar em criar quizz, a tela 3A (info basicas) s
     const screen3 = document.querySelector('.screen3A');
     screen3.classList.add('show-screen');
 }
+
+function continueQuestions() { // Ao clicar em prosseguir perguntas, a tela 3B (perguntas) será apresentada
+    alert('oioi');
+    const screen3 = document.querySelector('.screen3B');
+    screen3.classList.add('show-screen');
+}
+
+function continueLevel() { // Ao clicar em cprosseguir níveis, a tela 3C (níveis) será apresentada
+    alert('deu certo');
+    const screen3 = document.querySelector('.screen3C');
+    screen3.classList.add('show-screen');
+}
+
+function falha(){
+    alert('Por favor, preencha os dados corretamente!');
+}
+
+
+function showQuizz(){
+    /////
+}
