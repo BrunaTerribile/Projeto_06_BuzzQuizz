0c09c5b379f07653027bad0f1677da92bfc34722
luskamusca <luskamuskammouse@gmail.com>
2022-09-04 21:37:43 -0300

    parte da criação dos quizzes iniciada e melhoras no layout

--- script.js
+++ script.js
@@ -5,21 +5,27 @@ let allquizzes = [];
 //
 
 function createQuizz() { // Ao clicar em criar quizz, a tela 3A (info basicas) será apresentada
-    alert('Oi');
-    const screen3 = document.querySelector('.screen3A');
-    screen3.classList.add('show-screen');
+    alert('deu certo');
+    const screen3A = document.querySelector('.screen3A');
+    screen3A.classList.add('show-screen');
 }
 
 function continueQuestions(){ //Ao clicar em prosseguir para criar perguntas, a tela 3B (perguntas) será apresentada
-    alert('oioi');
-    const screen3 = document.querySelector('.screen3B');
-    screen3.classList.add('show-screen');
+    alert('deu certo');
+    const screen3B = document.querySelector('.screen3B');
+    screen3B.classList.add('show-screen');
 }
 
-function continueLevel() { // Ao clicar em prosseguir níveis, a tela 3C (níveis) será apresentada
+function continueLevels() { // Ao clicar em prosseguir níveis, a tela 3C (níveis) será apresentada
     alert('deu certo');
-    const screen3 = document.querySelector('.screen3C');
-    screen3.classList.add('show-screen');
+    const screen3C = document.querySelector('.screen3C');
+    screen3C.classList.add('show-screen');
+}
+
+function continueSucess() { // Ao clicar em prosseguir níveis, a tela 3D (sucesso) será apresentada
+    alert('deu certo');
+    const screen3D = document.querySelector('.screen3D');
+    screen3D.classList.add('show-screen');
 }
 
 function falha(){
@@ -65,4 +71,77 @@ function colocarQuizz(){ //Insere os quizzes na tela do usuário
 
 colocarQuizz()
 
-//aaaaaaaaaaa
+function sendCreatedQuizz(){
+    const promise2 = axios.post("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes", 
+    {
+        title: "Título do quizz",
+        image: "https://http.cat/411.jpg",
+        questions: [
+            {
+                title: "Título da pergunta 1",
+                color: "#123456",
+                answers: [
+                    {
+                        text: "Texto da resposta 1",
+                        image: "https://http.cat/411.jpg",
+                        isCorrectAnswer: true
+                    },
+                    {
+                        text: "Texto da resposta 2",
+                        image: "https://http.cat/412.jpg",
+                        isCorrectAnswer: false
+                    }
+                ]
+            },
+            {
+                title: "Título da pergunta 2",
+                color: "#123456",
+                answers: [
+                    {
+                        text: "Texto da resposta 1",
+                        image: "https://http.cat/411.jpg",
+                        isCorrectAnswer: true
+                    },
+                    {
+                        text: "Texto da resposta 2",
+                        image: "https://http.cat/412.jpg",
+                        isCorrectAnswer: false
+                    }
+                ]
+            },
+            {
+                title: "Título da pergunta 3",
+                color: "#123456",
+                answers: [
+                    {
+                        text: "Texto da resposta 1",
+                        image: "https://http.cat/411.jpg",
+                        isCorrectAnswer: true
+                    },
+                    {
+                        text: "Texto da resposta 2",
+                        image: "https://http.cat/412.jpg",
+                        isCorrectAnswer: false
+                    }
+                ]
+            }
+        ],
+        levels: [
+            {
+                title: "Título do nível 1",
+                image: "https://http.cat/411.jpg",
+                text: "Descrição do nível 1",
+                minValue: 0
+            },
+            {
+                title: "Título do nível 2",
+                image: "https://http.cat/412.jpg",
+                text: "Descrição do nível 2",
+                minValue: 50
+            }
+        ]
+    }
+    )
+}
+
+sendCreatedQuizz();
